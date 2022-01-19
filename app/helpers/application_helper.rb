7c178e73e71c9906afdd903e2cc7cc57663d73b7
Gabe Olivares <gabeolivares@Gabes-MBP-SourceDay.local>
2022-01-19 17:05:26 -0600

    Adding fix for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,6 @@ module ApplicationHelper
         description: 'Locate a Brewery anywhere in the world',
         keywords: 'beer, brewery, bar, drink, drinks, craftbeer, craft beer,locate, brewery, locateabrewery, brewhouse, pub, breweries near ne, ipa, types of beer, beer stores near me, microbrewery, beer gardens, brew pubs, biergarten',
         noindex: !Rails.env.production?,
-        icon: [
-            { href: image_url('favicon.ico') },
-            { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-        ],
         og: {
             site_name: 'Locate-A-Brewery',
             title: 'Locate A Brewery',
